c2a0f9b3397b12a0f804f4f008282c4bf2d1484e
Richard Blechinger <me@blechi.at>
2018-07-29 20:47:33 +0200

    :tada: Release Version 1.0
    This sets up everything to be ready for use by everyone. At the same time it serves as initial commit.

---
+++ src/pages/page-2.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+
+const SecondPage = () => (
+  <Layout>
+    <h2>👀 Hello from the second page.</h2>
+    <p>Pretty, isn't it?</p>
+  </Layout>
+)
+
+export default SecondPage
